dfa8f8a09a441a2687309808daf405a4f049a52f
Roman Kirpa <ghostdrs@gmail.com>
2018-06-07 23:45:31 +0300

    create project; add test controller;

---
+++ Wishlist/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wishlist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetPage()
+        {
+            string html = string.Empty;
+            string url = @"https://rozetka.com.ua/apple_iphone_x_64gb_silver/p22726294/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            ViewBag.Page = html;
+            return View();
+        }
+    }
+}
---
+++ Wishlist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wishlist.MvcApplication" Language="C#" %>
